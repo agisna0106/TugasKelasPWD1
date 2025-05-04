7b202330c0b8be986b5ffe7fb8e2895c4e1ffa06
agisna <agiiis0106@gmail.com>
2025-05-04 08:19:42 +0700

    commit from laptop

--- index.php
+++ index.php
@@ -1,2 +1,39 @@
 <?php
+include 'header.php';
+include 'nav.php';
+?>
+<section class="hero">
+    <h2>Selamat Datang</h2>
+    <p>Temukan informasi lengkap dan menarik tentang motor klasik, cruiser, dan retro disini.</p>
+</section>
 
+<section  class="motor-grid">
+    <div class="motor-card">
+        <a href="#">
+            <img src="" alt="Benelli Patagonian Eagle 250">
+            <h3>Benelli Patagonian Eagle 250</h3>
+        </a>
+    </div>
+    <div class="motor-card">
+        <a href="#">
+            <img src="" alt="Benelli Patagonian Eagle 250">
+            <h3>Benelli Patagonian Eagle 250</h3>
+        </a>
+    </div>
+    <div class="motor-card">
+        <a href="#">
+            <img src="" alt="Benelli Patagonian Eagle 250">
+            <h3>Benelli Patagonian Eagle 250</h3>
+        </a>
+    </div>
+    <div class="motor-card">
+        <a href="#">
+            <img src="" alt="Benelli Patagonian Eagle 250">
+            <h3>Benelli Patagonian Eagle 250</h3>
+        </a>
+    </div>
+</section>
+
+<?php
+include 'footer.php';
+?>
---
+++ nav.php
@@ -0,0 +1,9 @@
+<header>
+    <h1>Motor Klasik & Cruiser</h1>
+    <nav>
+        <a href="benelli.php">Benelli</a>
+        <a href="yamaha.php">Yamaha</a>
+        <a href="kawasaki.php">Kawasaki</a>
+        <a href="honda.php">Honda</a>
+    </nav>
+</header>
